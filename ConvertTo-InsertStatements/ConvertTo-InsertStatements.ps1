df5c9593bcdacd9dbd18f18d4d6eed337c50d5cc
Phil Factor <philipFactor@gmail.com>
2022-08-01 11:59:02 +0100

    Various improvements to ConvertTo-InsertStatements

--- ConvertTo-InsertStatements/ConvertTo-InsertStatements.ps1
+++ ConvertTo-InsertStatements/ConvertTo-InsertStatements.ps1
@@ -3,19 +3,36 @@
 		Converts a tabular object to a multi-row insert statement
 	
 	.DESCRIPTION
-		A quick way to create multi-row insert statements from an array of objects that all  have the same keys in the same order for every row.  You'd get this from reading in a JSON result.
+		A quick way to create multi-row insert statements from an array of objects that all  
+		have the same keys in the same order for every row.  You'd get this from reading 
+		in a JSON result. You can specify a list of one or more columns to exclude, and 
+		can provide a SQL at the start and end. 
 	
 	.PARAMETER TheObject
-		an array of psCustomObects, each of which have the same noteproperty names for the Nvalues
+		an array of psCustomObjects, each of which have the same noteproperty names for the 
+		column values
 	
 	.PARAMETER TheTableName
 		A description of the TheTableName parameter.
 	
-	.PARAMETER
-		A description of the  parameter.
+	.PARAMETER Batch
+		how many lines should we batch up in an insert statement. (performance)
+	
+	.PARAMETER Exclude
+		list of columns in the table  to exclude from the insert.
+	
+	.PARAMETER Prequel
+		A sql command to include before the insert. This can include a placeholder 
+		for the name of the table, (${table}) so you can create a looping construct 
+		with the same SQL
+	
+	.PARAMETER Sequel
+		A sql command to append  after the end of the insert.This can include a placeholder
+		for the name of the table, (${table}) so you can create a looping construct
+		with the same SQL
 	
 	.EXAMPLE
-		PS C:\> ConvertTo-InsertStatements - $value1
+		PS C:\> ConvertTo-InsertStatements
 	
 	.NOTES
 		Additional information about the function.
@@ -34,26 +51,41 @@ function ConvertTo-InsertStatements
 		[string]$TheTableName,
 		[Parameter(Mandatory = $false,
 				   Position = 3)]
-		[int]$Batch =500
+		[int]$Batch = 500,
+		[Parameter(Mandatory = $false,
+				   Position = 4)]
+		[Array]$exclude = @(),
+		[Parameter(Mandatory = $false,
+				   Position = 5)]
+		[string]$Prequel = $null,
+		[Parameter(Mandatory = $false,
+				   Position = 6)]
+		[string]$Sequel = $null
 	)
-
-$TheObject | ForEach-Object -Begin{$lines=@();$ii=$Batch;} -Process{
-    $line=$_; 
-    $LineProperties=$line.PSObject.Properties
-    if ($ii -eq $batch) {
-        $Names=$LineProperties |foreach{$_.Name}
-        #start a new query (batch them up into rows - there is an optimal amount)
-        "INSERT INTO $table ($($Names -join ', '))`r`n  VALUES"
-        $Beginning = $false;
-        }
-    $Values= $LineProperties|foreach{
-        if ($_.TypeNameOfValue -eq 'System.String'){'"'+$_.Value+ '"'} else {$_.Value}
-        } 
-    $lines+="($($Values -join ', '))";
-    if ($ii-- -eq 0) {
-        ($Lines -join ",`r`n")+';'
-        $ii=$Batch;
-        $lines=@();
-        }
-    }  -end{($Lines -join ",`r`n")+';'}
+	
+	if ($Prequel -ne $null) { "$Prequel" -ireplace '\${table}', $TheTableName };
+	$TheObject | ForEach-Object -Begin { $lines = @(); $ii = $Batch; } -Process {
+		$line = $_;
+		$LineProperties = $line.PSObject.Properties
+		if ($ii -eq $batch)
+		{
+			$Names = $LineProperties | foreach{ $_.Name }|where {$_ -notin $exclude}
+			#start a new query (batch them up into rows - there is an optimal amount)
+			"INSERT INTO $table ($($Names -join ', '))`r`n  VALUES"
+			$Beginning = $false;
+		}
+		$Values = $LineProperties | where {$_.Name -notin $exclude} | foreach{
+			if ($_.TypeNameOfValue -eq 'System.String') { '"' + $_.Value + '"' }
+			else { $_.Value }
+		}
+		$lines += "($($Values -join ', '))";
+		if ($ii-- -eq 0)
+		{
+			($Lines -join ",`r`n") + ';'
+			$ii = $Batch;
+			$lines = @();
+		}
+	} -end { ($Lines -join ",`r`n") + ';' }
+	
+	if ($Sequel -ne $null) { "$Sequel" -ireplace '\${table}', $TheTableName };
 }
---
+++ GenerateRandomText/Generate-NormallyDistributed.ps1
@@ -0,0 +1,35 @@
+﻿<#
+	.SYNOPSIS
+		return normally-distributed random number
+	
+	.DESCRIPTION
+		This is the Box-Muller transform
+	
+	.PARAMETER mean
+		the mean of the bellcurve.
+	
+	.PARAMETER stdDev
+		The standard deviation.
+	
+#>
+function Generate-NormallyDistributed
+{
+	[OutputType([double])]
+	param
+	(
+		[Parameter(Mandatory = $false)]
+		$mean = '0',
+		[Parameter(Mandatory = $false)]
+		$stdDev = '1'
+	)
+	
+	[double]$u1 = Get-Random -Minimum 0.0 -Maximum 1.0
+	[double]$u2 = Get-Random -Minimum 0.0 -Maximum 1.0
+	
+	[double]$randStdNormal = [math]::Sqrt(-2.0 * [math]::Log($u1)) * [math]::Sin(2.0 * [math]::PI * $u2)
+	[double]$randNormal = $mean + $stdDev * $randStdNormal
+	
+	$randNormal
+}
+
+ 
