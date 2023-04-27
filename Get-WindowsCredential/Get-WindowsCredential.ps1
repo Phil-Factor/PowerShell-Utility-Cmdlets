b8796979532480b40e4c80d291458c5532b5b572
Phil Factor <philipFactor@gmail.com>
2023-04-27 14:46:46 +0100

    Update Get-WindowsCredential.ps1

--- Get-WindowsCredential/Get-WindowsCredential.ps1
+++ Get-WindowsCredential/Get-WindowsCredential.ps1
@@ -9,7 +9,19 @@
 		Windows, including access to the credential manager.  My part is to write the PowerShell to access the
 		credentials, and query the user if the password can't be found within credential manager.
 	.EXAMPLE
+        #get or set the user philFactor for server 'Philf021'
 		Get-WindowsCredential -User 'PhilFactor' -Server 'Philf021'
+
+        #get or set the user philFactor for server 'Philf021' for sqlserver
+		Get-WindowsCredential -User 'PhilFactor' -Server 'Philf021' -RDBMS='sqlserver'
+
+        #get or set the user philFactor  saved with the token 'mysecret'
+		Get-WindowsCredential -User 'PhilFactor' -token 'mysecret'
+
+        #get the user philFactor  saved with the token 'mysecret'
+        #add the userid when you set it
+		Get-WindowsCredential -token 'mysecret'
+
 	
 	.PARAMETER User
 		The user of the login.
@@ -20,6 +32,14 @@
 	.PARAMETER RDBMS
 		the RDBMS that is being served (usually the jdbc string). This is important only if you have one
 		server that is hosting several relational systems.
+
+	.PARAMETER Token
+        if you wish to use a token rather than use lots of parameters that give away your userid
+        you can use a token to save your credentials.
+
+	.PARAMETER Conf
+        You want the output in flyway.conf format.
+
 	
 	.NOTES
 		Additional information about the function.
@@ -29,12 +49,27 @@ function Get-WindowsCredential
 	[CmdletBinding()]
 	param
 	(
-		[Parameter(Mandatory = $true)]
-		$User,
-		[Parameter(Mandatory = $true)]
-		$Server,
-		$RDBMS = $null
+		$User = $null,
+		#the name of the user id
+
+		$Server = $null,
+		#the name of the server
+
+		$RDBMS = $null,
+		#the name of the RDBMS
+
+		$Token = $null,
+		#if you like to use a Token instead, to provide a password
+
+		$Conf = $false #if you want a flyway-formatted password configuration
 	)
+	
+	if ($User -eq $null -or $Server -eq $null)
+	{
+		if ($Token -eq $null)
+		{ write-Error "You must provide at least a -User and -Server, or else use a token as parameter" }
+	}
+	
 	
 	# Loads SSH Password from Windows Credential Manager
 	
@@ -114,11 +149,16 @@ namespace CredManager {
   }
 }
 "@
-	# thanks to Mavaddat Javid
-	if ([string]::IsNullOrEmpty($rdbms))
+	# thanks to Mavaddat Javid and others  for that code
+	if (!([string]::IsNullOrEmpty($Token)))
+	{
+		$secret = $Token;
+	}
+	elseif ([string]::IsNullOrEmpty($rdbms))
 	{ $secret = "$server-$user" }
 	else
 	{ $secret = "$rdbms-$server-$user" }
+	
 	$ItExists = $false #until proven otherwise
 	try
 	{
@@ -133,28 +173,38 @@ namespace CredManager {
 		}
 		catch
 		{
-			write-warning "cannot get Credential for $user to access $rdbms $server";
+			write-warning "cannot get Credential for $Secret";
 		}
 	}
 	
 	if (!($ItExists))
 	{
-		Get-Credential -Message "Please provide a password for user to access $rdbms on $server" -UserName $user |
+		if ([string]::IsNullOrEmpty($Token))
+		{ $PromptMessage = "Please provide a user and password for token $Token "; }
+		else
+		{ $PromptMessage = "Please provide a password for user $user on server $Server" }
+		
+		Get-Credential -Message $PromptMessage -UserName $user |
 		foreach {
 			write-verbose "$secret, $($_.UserName), $($_.GetNetworkCredential().password)"
 			[CredManager.Util]::SetUserCredential($secret, $_.UserName, $_.GetNetworkCredential().password)
 		}
 	}
-	else
+	try
 	{
-		try
-		{
-			$OurCredential = [CredManager.Util]::GetUserCredential($secret);
-		}
-		catch
-		{
-			write-warning "cannot get Credential for $user to access $rdbms $server";
-		}
+		$OurCredential = [CredManager.Util]::GetUserCredential($secret);
 	}
-	[pscustomobject]@{ 'user' = $OurCredential.Username; 'Password' = $OurCredential.password; }
+	catch
+	{
+		write-warning "cannot get Credential for $user to access $rdbms $server using token '$secret'";
+	}
+	if ($conf -eq $false) # just send the object so they can fill in userid and password
+	{
+		
+		[pscustomobject]@{ 'user' = $OurCredential.Username; 'Password' = $OurCredential.password; }
+	}
+	else # just send the flyway.conf format for the credential
+	{
+		write-output "flyway.user=$($OurCredential.Username)`nflyway.password=$($OurCredential.password)`n"
+	}
 }
