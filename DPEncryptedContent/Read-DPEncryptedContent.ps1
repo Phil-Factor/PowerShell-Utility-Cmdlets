27a91bea2a562f590a263368ce0d8795e573e3be
Phil Factor <philipFactor@gmail.com>
2023-05-11 09:55:30 +0100

    Saved new encryption Cmdlets

---
+++ DPEncryptedContent/Read-DPEncryptedContent.ps1
@@ -0,0 +1,41 @@
+﻿<#
+	.SYNOPSIS
+		returns the contents of an encrypted file in the user area.
+	
+	.DESCRIPTION
+		This PowerShell routine will fetch the contents of a file
+		containing an encrypted object stored in an XML Document
+		This is encrypted as a system.secure.string. It converts
+		it back into plain text
+	
+	.PARAMETER Filename
+		A description of the Server parameter.
+	
+	.NOTES
+		Additional information about the function.
+#>
+function Read-DPEncryptedContent
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   HelpMessage = 'The File to decrypt')]
+		$Filename
+	)
+	
+	$secureString = Import-Clixml $Filename
+	$bstrPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
+	try
+	{
+		$originalText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstrPtr)
+	}
+	Catch { write-error "sadly we couldn't get the unencrypted contents of  $Filename" }
+	finally
+	{
+		[System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstrPtr)
+	}
+	Write-Output $originalText
+}
+
---
+++ DPEncryptedContent/Write-DPEncryptedContent.ps1
@@ -0,0 +1,38 @@
+﻿<#
+	.SYNOPSIS
+		Writes the string as an XML file in the user area with the 
+		given filename.
+	
+	.DESCRIPTION
+		This PowerShell routine will save the string in its
+		encrypted form as a system.secure.string an XML file
+		
+	.EXAMPLE
+Write-DPEncryptedContent 'Fee-fi-fo-fum,
+I smell the blood of an Englishman,
+Be he alive, or be he dead
+I''ll grind his bones to make my bread' "$env:USERPROFILE\SecretKey"
+	
+	.PARAMETER String
+		the string that you wish to encrypt.
+	
+	.PARAMETER Filename
+		The file you want to save the encrypted text in
+	
+
+#>
+function Write-DPEncryptedContent
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'the string that you wish to encrypt.')]
+		[String]$String,
+		[Parameter(Mandatory = $true,
+				   HelpMessage = 'The file you want to save the encrypted text in')]
+		[String]$Filename
+	)
+	
+	$String | ConvertTo-SecureString -AsPlainText -Force | Export-Clixml  $Filename -Force
+}
